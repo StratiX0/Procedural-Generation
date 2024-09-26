7c77d09f93e58243257809ad62275feab66f6970
StratiX0 <jerome.hussenet@sfr.fr>
2024-09-26 17:27:38 +0200

    refactor: -gizmos

--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -153,13 +153,13 @@ public class MeshGenerator : MonoBehaviour
         mesh.RecalculateNormals();
     }
 
-    private void OnDrawGizmos()
-    {
-        if (vertices == null) return;
+    //private void OnDrawGizmos()
+    //{
+    //    if (vertices == null) return;
 
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            Gizmos.DrawSphere(vertices[i], 0.1f);
-        }
-    }
+    //    for (int i = 0; i < vertices.Length; i++)
+    //    {
+    //        Gizmos.DrawSphere(vertices[i], 0.1f);
+    //    }
+    //}
 }
