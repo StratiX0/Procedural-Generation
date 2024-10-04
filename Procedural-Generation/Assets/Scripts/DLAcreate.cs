7b866ac5464d03ed2b8d273b5e77d0c9237db76d
Kagutsuchi84 <115561782+Mattys8423@users.noreply.github.com>
2024-10-04 23:36:46 +0200

    Add + Fix
    Add comntary on the code DLAcreate and DLAplayer + Fix slider 2 on the DLA

--- Procedural-Generation/Assets/Scripts/DLAcreate.cs
+++ Procedural-Generation/Assets/Scripts/DLAcreate.cs
@@ -7,12 +7,13 @@ using UnityEngine;
 
 public class DLACreate : MonoBehaviour
 {
+    //Instantiate variable
     public Transform parent;
     public GameObject inputPrefab;
-    List<GameObject> allPrefabs = null;
-    public int DLAgrowthcount = 1000;
     private GameObject activePrefab = null;
     private GameObject DLAGenerator = null;
+    List<GameObject> allPrefabs = null;
+    public int DLAgrowthcount = 1000;
     public float Seconds = 0;
     public bool is2D = false;
     public bool Horizontal = false;
@@ -23,14 +24,17 @@ public class DLACreate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Restart the Scene
         SetupScene();
     }
 
+    //Update is called constantly
     private void Update()
     {
 
     }
 
+    //Time to wait between each cube spawn
     public IEnumerator RunDLA()
     {
         for (int i = 0; i < DLAgrowthcount; i++)
@@ -40,12 +44,15 @@ public class DLACreate : MonoBehaviour
         }
     }
 
+    //
     public void SetupScene()
     {
+        //Search if there is any prefabs in allPrefabs List
         if (allPrefabs != null)
         {
             if (allPrefabs.Count > 0)
             {
+                //Destroy every prefab
                 foreach (var pf in allPrefabs)
                 {
                     Destroy(pf);
@@ -54,35 +61,46 @@ public class DLACreate : MonoBehaviour
             }
         }
 
+        //Create the main prefab in center an add it to the list.
         allPrefabs = new List<GameObject>();
         GameObject newPrefab = Instantiate(inputPrefab, Vector3.zero, Quaternion.identity);
+        //Set parent GameObject as Parent of the newPrefab
         newPrefab.transform.SetParent(parent, true);
         allPrefabs.Add(newPrefab);
 
+        //Create a Prefab "Hover" to see what is the last prefab whiwh has spawn.
         if (activePrefab == null)
         {
             activePrefab = Instantiate(inputPrefab, Vector3.zero, Quaternion.identity);
+            //Set parent GameObject as Parent of the newPrefab
             activePrefab.transform.SetParent(parent, true);
             activePrefab.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+            //Set color red
             activePrefab.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 0.15f, 0.15f);
         }
         activePrefab.transform.position = newPrefab.transform.position;
 
+        //Spawn every cube create by the generator (fonction create after).
         if (DLAGenerator == null)
         {
             DLAGenerator = Instantiate(inputPrefab, Vector3.zero, Quaternion.identity);
+            //Set parent GameObject as Parent of the newPrefab
             DLAGenerator.transform.SetParent(parent, true);
             DLAGenerator.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            //Set the color pink.
             DLAGenerator.GetComponent<MeshRenderer>().material.color = new Color(1.15f, 0.15f, 1.0f);
         }
     }
 
+    //Create cubes randomly.
     void AddDLAAgent()
     {
         if (activePrefab != null)
         {
+            //Define Random vector.
             Vector3 DLADirection = new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * 15;
 
+            //If there is less prefab than it should be, define the Direction and Create a cube then attach it to the closestgeo.
             if (allPrefabs.Count < DLAgrowthcount)
             {
                 Vector3 DLAtester = DLADirection;
@@ -110,16 +128,21 @@ public class DLACreate : MonoBehaviour
         }
     }
 
+    //Find the closest Prefab.
     private GameObject GetClosestGeo(Vector3 testerVec, List<GameObject> allPrefabs)
     {
         GameObject closestGeo = null;
+
         if (allPrefabs.Count > 0)
         {
             float closestDistance = 1000f;
+
             for (int i = 0; i < allPrefabs.Count; i++)
             {
                 GameObject currentGeo = allPrefabs[i];
+                //Vector of the distance between testerVec and any prefab.
                 float dist = Vector3.Distance(testerVec, currentGeo.transform.position);
+                //If the new distance is smaller but superior at 0.5f, then update the closestGeo Gameobject and closestDistance.
                 if (dist < closestDistance && dist > 0.5f)
                 {
                     closestGeo = currentGeo;
@@ -130,6 +153,7 @@ public class DLACreate : MonoBehaviour
         return closestGeo;
     }
 
+    //Add one prefab at a position define in the function, and update the position of activePrefab.
     public void AddOnePrefab(GameObject testGameObj, Vector3 growDir)
     {
         growDir.Normalize();
--- Procedural-Generation/Assets/Scripts/PlayerDLA.cs
+++ Procedural-Generation/Assets/Scripts/PlayerDLA.cs
@@ -10,16 +10,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If the mouse bouton is clickedand start the coroutine.
         if (Input.GetMouseButtonDown(0))
         {
+            //Find where it is clicked
             RaycastHit hit;
             var MousePosition = Input.mousePosition;
             var Ray = Camera.main.ScreenPointToRay(MousePosition);
 
             if (Physics.Raycast(Ray, out hit))
             {
+                //If it is on the existant Prefab 
                 var CurrentObject = hit.collider.gameObject;
+                //Add a cube
                 dlaCreate.AddOnePrefab(CurrentObject, hit.normal.normalized);
+                //Start coroutine (DLA Algorithme)
                 StartCoroutine(dlaCreate.RunDLA());
             }
         }
